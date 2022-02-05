d369399fc30ed6229505444883a90e8753647878
Prasad Arbad <prasadarbad9@gmail.com>
2022-02-05 20:34:41 +0530

    day1

---
+++ lib/answer.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'dart:core';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+  Answer(
+    this.selectHandler,
+    this.answerText,
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
